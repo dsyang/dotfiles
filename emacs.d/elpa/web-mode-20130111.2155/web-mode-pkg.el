382ec9ab37b602eac96c7cb91daa0d5644929c7d
Dan Yang <dsyang92@gmail.com>
2013-02-24 19:39:11 -0500

    updated elpa packages

--- emacs.d/elpa/web-mode-20130111.2155/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20130111.2155" "major mode for editing HTML templates [source: github]" (quote nil))
---
+++ emacs.d/elpa/web-mode-20130220.2157/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20130220.2157" "major mode for editing HTML templates [source: github]" (quote nil))
