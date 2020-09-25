ffe771061ba5db3a9854608a43ec7da47dba968a
Dan Yang <dsyang92@gmail.com>
2020-09-25 00:00:14 -0700

    add install.sh for use with github codespaces

---
+++ install.sh
@@ -0,0 +1,7 @@
+# link everyhing
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/emacs.d ~/.emacs.d
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/tmux/tmux.d ~/.tmux.d
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+# link bash setup from chromebook
